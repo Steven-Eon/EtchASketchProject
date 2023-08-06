89918150f7667c945a61c4c812c8682922aa2ad9
Steven-Eon <91300742+Steven-Eon@users.noreply.github.com>
2023-08-06 04:00:12 -0700

    Grid generation prompt successfully implemented.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function initializeGrid() {
-    const sketchGrid = document.querySelector(".flexColumn, sketchContainer");
+function initializeGrid(sizeX, sizeY) {
+    const sketchGrid = document.querySelector(".flexColumn .sketchContainer");
     const gridRow = document.createElement('div');
     const gridElement = document.createElement('div');
 
     gridRow.classList.toggle("flexRow");
     gridElement.classList.toggle("gridElement");
 
-    for (let i = 0; i < 16; ++i)
+    for (let i = 0; i < sizeY; ++i)
     {
         gridRow.appendChild(gridElement.cloneNode(true));
     }
-    for (let i = 0; i < 16; ++i)
+    for (let i = 0; i < sizeX; ++i)
     {
         sketchGrid.appendChild(gridRow.cloneNode(true));
     }
@@ -19,11 +19,38 @@ function initializeGrid() {
 
     nodeList.forEach((node) => {
         node.addEventListener("mouseover", () => {
-            node.classList.toggle("gridElementHover")
+            node.classList.add("gridElementHover")
+        });
+
+        node.addEventListener("mouseout", () => {
+            node.classList.remove("gridElementHover");
         });
     })
+}
+
+function promptSize() {
+    let sizeX = prompt("Specify desired size of X-axis (Limit: 100)");
+    while (sizeX < 1 || sizeX > 100)
+    {
+        sizeX = prompt("Invalid input, please try again.");
+    }
+
+    let sizeY = prompt("Specify desired size of Y-axis (Limit: 100)");
+    while (sizeY < 1 || sizeY > 100)
+    {
+        sizeY = prompt("Invalid input, please try again.");
+    }
+
+    clearGrid();
+    initializeGrid(sizeX, sizeY);
 
 
 }
 
-initializeGrid();
+function clearGrid() {
+    const gridToDelete = document.querySelector(".sketchContainer");
+    while (gridToDelete.firstChild)
+    {
+        gridToDelete.removeChild(gridToDelete.firstChild);
+    }
+}
